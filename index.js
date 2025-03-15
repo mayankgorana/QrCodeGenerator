11d83aee3020436eb1aee9e2729148d7c6a1e87b
Mayank <mayankgorana2003@gmail.com>
2025-03-15 23:13:46 +0530

    Initial commit - QR Code Generator setup

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-import inquirer from 'inquirer';
-import qr from 'qr-image';
-import fs from 'fs';
+import express from "express";
+import cors from "cors";
+import qr from "qr-image";
+import fs from "fs";
 
-inquirer
-    .prompt([
-        {
-            message: "Type in your URL",
-            name: "URL"
-        }
-    ])
-    .then((answers) => {
-        
-        const url = answers.URL;
-        var qr_svg = qr.image(url);
-        qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-        fs.writeFile('Url.txt', url, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-          }); 
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-            
-        } else {
-           
+app.post("/generate", (req, res) => {
+    const { url } = req.body;
+    if (!url) return res.status(400).json({ error: "URL is required" });
+
+    try {
+        // Generate QR code
+        const qr_svg = qr.image(url, { type: "png" });
+        const filePath = "qr_code.png";
+
+        // Save to file
+        const writeStream = fs.createWriteStream(filePath);
+        qr_svg.pipe(writeStream);
+
+        writeStream.on("finish", () => {
+            res.json({ 
+                message: "QR Code generated successfully!", 
+                downloadUrl: `http://localhost:5000/download` 
+            });
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "Failed to generate QR Code" });
+    }
+});
+
+// Route to download the generated QR Code
+app.get("/download", (req, res) => {
+    const filePath = "qr_code.png";
+    res.download(filePath, "qr_code.png", (err) => {
+        if (err) {
+            res.status(500).json({ error: "Error downloading the file" });
         }
     });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
